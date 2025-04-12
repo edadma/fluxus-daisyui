39b44d7dfefd765d35c3856541212850b5245664
Edward A Maxedon <edadma@gmail.com>
2025-04-12 15:26:16 -0400

    Sidebar

--- examples/src/main/scala/io/github/edadma/fluxus/daisyui/examples/SidebarTest.scala
+++ examples/src/main/scala/io/github/edadma/fluxus/daisyui/examples/SidebarTest.scala
@@ -236,29 +236,32 @@ def SidebarTest: FluxusNode = {
         "Sidebar Component Demo",
       ),
 
-      // Alert for navigation events
-      if (showAlert) {
-        div(
-          cls := "alert alert-info mb-4",
+      // Fixed height alert area to prevent layout jumping
+      div(
+        cls := "h-16 mb-4", // Fixed height container
+        if (showAlert) {
           div(
-            cls := "flex-1",
-            svg(
-              xmlns   := "http://www.w3.org/2000/svg",
-              fill    := "none",
-              viewBox := "0 0 24 24",
-              stroke  := "currentColor",
-              cls     := "w-6 h-6 mx-2",
-              path(
-                strokeLinecap  := "round",
-                strokeLinejoin := "round",
-                strokeWidth    := "2",
-                d              := "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
+            cls := "alert alert-info",
+            div(
+              cls := "flex-1",
+              svg(
+                xmlns   := "http://www.w3.org/2000/svg",
+                fill    := "none",
+                viewBox := "0 0 24 24",
+                stroke  := "currentColor",
+                cls     := "w-6 h-6 mx-2",
+                path(
+                  strokeLinecap  := "round",
+                  strokeLinejoin := "round",
+                  strokeWidth    := "2",
+                  d              := "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
+                ),
               ),
+              label(alertMessage),
             ),
-            label(alertMessage),
-          ),
-        )
-      } else null,
+          )
+        } else null,
+      ),
       div(
         cls := "flex flex-wrap md:flex-nowrap gap-8",
 
